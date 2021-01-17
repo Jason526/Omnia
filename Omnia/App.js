7f298e6d7bb4e3da6234021419b20424f5cda8b5
palvisha13 <60575398+palvisha13@users.noreply.github.com>
2021-01-17 00:05:08 -0500

    Updated App.js

--- Omnia/App.js
+++ Omnia/App.js
@@ -4,39 +4,27 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./src/screens/Home.js";
 import Job from "./src/screens/Job.js";
-import LoginScreen from "./src/screens/LoginScreen.js";
-import RegistrationScreen from "./src/screens/RegistrationScreen.js";
+import LoginScreen from "./src/screens//LoginScreen/LoginScreen.js";
+import RegistrationScreen from "./src/screens/RegistrationScreen/RegistrationScreen.js";
 import SettingsPage from "./src/screens/SettingsPage.js";
-import Choice from "./src/screens/Choice.js";
-import {Image} from "react-native";
+import 'react-native-gesture-handler';
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
+
+
+
+
 
 const Tab = createBottomTabNavigator();
 const Stack =createStackNavigator();
-
-function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 50, height: 40,resizeMode:'contain' }}
-      source={require("./assets/OmniaLogoWhite.png")}
-    />
-  );
-}
 
  function Main() {
 
   return (
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} 
-        options={{
-          headerTitle: props => <LogoTitle {...props} /> ,
-          headerStyle: {
-            height: 120,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
+        <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Job" component={Job} />
         <Tab.Screen name="SettingsPage" component={SettingsPage} />
       </Tab.Navigator>
@@ -48,50 +36,9 @@ function LogoTitle() {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} 
-        options={{
-          headerTitle: props => <LogoTitle {...props} /> ,
-          headerStyle: {
-            height: 120,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
-          <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} 
-        options={{
-          headerTitle: props => <LogoTitle {...props} /> ,
-          headerStyle: {
-            height: 120,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
-        <Stack.Screen name="Choice" component={Choice} 
-        options={{
-          headerTitle: props => <LogoTitle {...props} /> ,
-          headerStyle: {
-            height: 120,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
-          <Stack.Screen name="Main" component={Main} 
-        options={{
-          headerTitle: props => <LogoTitle {...props} /> ,
-          headerStyle: {
-            height: 120,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} />
+          <Stack.Screen name="Main" component={Main} />
         </Stack.Navigator>
       </NavigationContainer>
     );
