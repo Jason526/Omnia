ba9cf1127397cf5b24c441d9f212a2abf4b4d755
palvisha13 <60575398+palvisha13@users.noreply.github.com>
2021-01-17 00:27:23 -0500

    Updated index.js

--- Omnia/index.js
+++ Omnia/index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+export { default as LoginScreen } from './LoginScreen/LoginScreen'
 
+export { default as HomeScreen } from './HomeScreen/HomeScreen'
+
+export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen'
 AppRegistry.registerComponent(appName, () => App);
